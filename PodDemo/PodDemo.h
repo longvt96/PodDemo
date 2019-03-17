c82539a60b6884d65e6f84e41878fc08355c6f30
thanhlong12312 <thanhlong140496@gmail.com>
2019-03-17 10:43:10 +0700

    Initial project setup

---
+++ PodDemo/PodDemo.h
@@ -0,0 +1,19 @@
+//
+//  PodDemo.h
+//  PodDemo
+//
+//  Created by ThanhLong on 3/17/19.
+//  Copyright © 2019 ThanhLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodDemo.
+FOUNDATION_EXPORT double PodDemoVersionNumber;
+
+//! Project version string for PodDemo.
+FOUNDATION_EXPORT const unsigned char PodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodDemo/PublicHeader.h>
+
+
