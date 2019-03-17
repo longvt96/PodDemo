a266534a3f790baad25526a06bcc76eab00eb57c
thanhlong12312 <thanhlong140496@gmail.com>
2019-03-17 17:40:14 +0700

    update

--- PodDemo/PodDemo.swift
+++ PodDemo/PodDemo.swift
@@ -10,11 +10,11 @@
     
     let name = "PodDemo"
     
-    public func add(a: Int, b: Int) -> Int {
+    public static func add(a: Int, b: Int) -> Int {
         return a + b
     }
     
-    public func sub(a: Int, b: Int) -> Int {
+    public static func sub(a: Int, b: Int) -> Int {
         return a - b
     }
     
