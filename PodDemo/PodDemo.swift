a3b1ea0d9c342ce536a2ac777420a27bd610d281
thanhlong12312 <thanhlong140496@gmail.com>
2019-03-17 16:13:47 +0700

    add test case

---
+++ PodDemo/PodDemo.swift
@@ -0,0 +1,21 @@
+ //
+//  PodDemo.swift
+//  PodDemo
+//
+//  Created by ThanhLong on 3/17/19.
+//  Copyright © 2019 ThanhLong. All rights reserved.
+//
+
+ public final class PodDemo {
+    
+    let name = "PodDemo"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+ }
--- PodDemoTests/PodDemoTests.swift
+++ PodDemoTests/PodDemoTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class PodDemoTests: XCTestCase {
 
+    var podDemo: PodDemo!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        podDemo = PodDemo()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(podDemo.add(a: 1, b: 2), 3)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(podDemo.sub(a: 2, b: 1), 1)
     }
 
 }
